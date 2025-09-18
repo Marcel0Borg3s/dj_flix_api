425c36e5733dbfe692bf3921a695326ce144713f
Marcelo Borges <marcelosbgs@gmail.com>
2025-09-17 21:31:03 -0300

    atualização SO Ubuntu

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/MarceloBorges/Projects/Flix_api/venv
+set -gx VIRTUAL_ENV /home/mb/projects/dj_flix_api/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
