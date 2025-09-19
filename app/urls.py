66fd8b6b051bf6b576b8ef20c6343a0e1cdcf3af
Marcelo Borges <marcelosbgs@gmail.com>
2025-09-18 21:49:21 -0300

    atualização do bd

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
+from genres.views import genre_view
 
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('genres/', genre_view, name='genre-list'),
+    path('test/', lambda request: HttpResponse("Funcionando!")),
 ]
