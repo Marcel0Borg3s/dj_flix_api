7ea0569682a2d53267f13e1dc32f24ebb20b69f7
Marcelo Borges <marcelosbgs@gmail.com>
2025-09-23 21:45:14 -0300

    #121 config o detail

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
-from genres.views import genre_view
+from genres.views import genre_create_list_view, genre_detail_view
 
 from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre-list'),
-    path('test/', lambda request: HttpResponse("Funcionando!")),
+    path('genres/', genre_create_list_view, name='genre-create-list'),
+    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
 ]
+
+# 121 / 10:17 
+
