60632499d4d1aba7812c4e4ef5a479674c87854e
Marcelo Borges <marcelosbgs@gmail.com>
2025-10-17 05:43:26 -0300

    #144 SerializerMethodField

--- movies/serializers.py
+++ movies/serializers.py
@@ -4,9 +4,14 @@ from movies.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
 
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        return 5 
 
     def validate_release_date(self, value):
         if value.year < 1990:
@@ -17,4 +22,5 @@ class MovieSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Resume must not exceed 200 characters.")
         return value
     
-    
+
+
