3d0cf1a39208095d8cc64500f14718ec14a57570
Marcelo Borges <marcelosbgs@gmail.com>
2025-10-20 05:48:35 -0300

    #146 manual rate media

--- movies/serializers.py
+++ movies/serializers.py
@@ -11,7 +11,19 @@ class MovieSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_rate(self, obj):
-        return 5 
+        reviews = obj.reviews.all()
+
+        if reviews:
+            sum_reviews = 0
+
+            for review in reviews:
+                sum_reviews += review.stars
+
+            reviews_count = reviews.count()
+
+            return round(sum_reviews / reviews_count, 1)
+        
+        return "No reviews yet"
 
     def validate_release_date(self, value):
         if value.year < 1990:
