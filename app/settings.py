d0c22319b08d66a674cf072a553b0761d2c91664
Marcelo Borges <marcelosbgs@gmail.com>
2025-10-25 09:41:12 -0300

    #150 insert Authentication JWT

--- app/settings.py
+++ app/settings.py
@@ -29,8 +29,8 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'rest_framework_simplejwt',
-    'authentication',
     
+    'authentication',
     'genres',
     'actors',
     'movies',
@@ -119,3 +119,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
