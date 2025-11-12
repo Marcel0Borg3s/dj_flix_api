c0bc2d6eac26f7295d491bbd830c716a3da0938d
Marcelo Borges <marcelosbgs@gmail.com>
2025-11-12 06:04:54 -0300

    #163 Endpoint Stats

--- movies/views.py
+++ movies/views.py
@@ -1,16 +1,25 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 
 class MovieCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
 
+    def get(self, request):
+        return response.Response(
+            data={'message': 'Funcionando'},
+            status=status.HTTP_200_OK,
+        )
